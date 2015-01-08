5321f08ce9ff11eed8896691bb20e140811b96d8
Michael Drogalis <mjd3089@rit.edu>
2015-01-07 19:52:01 -0800

    client.

---
+++ src/http_server/client.clj
@@ -0,0 +1,18 @@
+(ns http-server.client
+  (:require [org.httpkit.server :refer [with-channel send! run-server]]
+            [org.httpkit.client :as http]))
+
+
+(defn -main [& args]
+  (let [uri (nth args 0)
+        n (Integer/parseInt (nth args 1))
+        t (Integer/parseInt (nth args 2))
+        drag (Integer/parseInt (nth args 3))]
+    (prn "Start")
+    (doseq [_ (range t)]
+      (future
+        (doseq [_ (range n)]
+          (http/get uri)
+          (Thread/sleep drag))))
+    (prn "Done")))
+
